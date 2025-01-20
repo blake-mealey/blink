874e7aa51ca868f4350b9b3563faf75ee50b9ace
Blake Mealey <blake.mealey@vercel.com>
2025-01-20 12:51:04 -0600

    add vercel analytics

--- src/app/[name]/route.ts
+++ src/app/[name]/route.ts
@@ -1,6 +1,7 @@
 import { getLink } from '@/lib/links';
 import { redis } from '@/lib/redis';
 import { notFound, redirect } from 'next/navigation';
+import { track } from '@vercel/analytics/server';
 
 export async function GET(
   _request: Request,
@@ -9,8 +10,16 @@ export async function GET(
   const { name } = await params;
   const link = await getLink(redis, name);
   if (!link) {
+    track('link-not-found', {
+      linkName: name,
+    });
     return notFound();
   }
+
+  track('link-redirect', {
+    linkName: name,
+    linkUrl: link.url,
+  });
 
   return redirect(link.url);
 }
