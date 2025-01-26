9c6b3d27768ba76b3a6d164b682cb5efb29913d3
Blake Mealey <blake.mealey@vercel.com>
2025-01-26 16:41:11 -0600

    move bookmark actions to 3-dot menu

--- src/app/admin/bookmarks/bookmarks-list.tsx
+++ src/app/admin/bookmarks/bookmarks-list.tsx
@@ -9,17 +9,18 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuLabel,
-  ContextMenuTrigger,
-} from '@/components/ui/context-menu';
 import { Bookmark } from '@/lib/bookmarks';
 import { Favicon, FaviconProps } from '@/lib/favicons';
-import { PencilIcon, TrashIcon } from 'lucide-react';
+import { EllipsisVerticalIcon, PencilIcon, TrashIcon } from 'lucide-react';
 import { removeBookmarkAction } from './bookmarks-actions';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
 
 const dateFormatter = new Intl.DateTimeFormat();
 
@@ -67,43 +68,47 @@ function BookmarksListItem({
         rel="noopener"
         className="group focus-visible:outline-none"
       >
-        <ContextMenu>
-          <ContextMenuTrigger>
-            <section className="transition-colors group-hover:bg-muted group-focus-visible:ring-1 group-focus-visible:ring-ring rounded-lg px-3 py-2 flex flex-col gap-2">
-              <header>
-                <div className="flex justify-between">
-                  <h1 className="font-semibold">{bookmark.graphMeta.title}</h1>
-                  <div className="text-muted-foreground text-sm font-medium text-nowrap">
-                    Added on{' '}
-                    {dateFormatter.format(new Date(bookmark.createdAt))}
-                  </div>
+        <section className="transition-colors group-hover:bg-muted group-focus-visible:ring-1 group-focus-visible:ring-ring rounded-lg px-3 py-2 flex flex-col gap-2">
+          <header>
+            <div className="flex justify-between items-center">
+              <h1 className="font-semibold">{bookmark.graphMeta.title}</h1>
+              <div className="flex items-center gap-2">
+                <div className="text-muted-foreground text-sm font-medium text-nowrap">
+                  Added on {dateFormatter.format(new Date(bookmark.createdAt))}
                 </div>
-                <div className="flex items-center gap-2 text-muted-foreground font-medium">
-                  <Favicon src={bookmark.faviconUrl} />
-                  {urlFormatter.format(bookmark.url)}
-                </div>
-              </header>
-
-              <div className="text-muted-foreground">
-                {bookmark.graphMeta.description}
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <EllipsisVerticalIcon />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <AlertDialogTrigger asChild>
+                      <DropdownMenuItem className="flex items-center gap-2">
+                        <TrashIcon size={16} /> Remove
+                      </DropdownMenuItem>
+                    </AlertDialogTrigger>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </div>
-              {bookmark.notes ? (
-                <div className="flex items-start gap-2">
-                  <PencilIcon size={16} className="mt-1" />
-                  <p className="text-muted-foreground">{bookmark.notes}</p>
-                </div>
-              ) : null}
-            </section>
-          </ContextMenuTrigger>
-          <ContextMenuContent>
-            <ContextMenuLabel>Actions</ContextMenuLabel>
-            <AlertDialogTrigger asChild>
-              <ContextMenuItem className="flex items-center gap-2">
-                <TrashIcon size={16} /> Remove
-              </ContextMenuItem>
-            </AlertDialogTrigger>
-          </ContextMenuContent>
-        </ContextMenu>
+            </div>
+            <div className="flex items-center gap-2 text-muted-foreground font-medium">
+              <Favicon src={bookmark.faviconUrl} />
+              {urlFormatter.format(bookmark.url)}
+            </div>
+          </header>
+
+          <div className="text-muted-foreground">
+            {bookmark.graphMeta.description}
+          </div>
+          {bookmark.notes ? (
+            <div className="flex items-start gap-2">
+              <PencilIcon size={16} className="mt-1" />
+              <p className="text-muted-foreground">{bookmark.notes}</p>
+            </div>
+          ) : null}
+        </section>
       </a>
 
       <AlertDialogContent>
